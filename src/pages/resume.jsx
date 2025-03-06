221156e6be560aaf14beb10116991a1b03725221
Adam Young <adamyoung1414@gmail.com>
2025-03-05 20:55:42 -0500

    moved technologies section to work from index page at Lauren's request updated resume per Lauren's feedback made it so download resume link actually triggers download on resume page

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -4,13 +4,13 @@ export function Resume() {
       <h1 className="text-2xl font-bold">Resume</h1>
       <div className="flex flex-col items-center p-6 rounded-xl">
         <img
-          src="/resume.png" // Corrected path for public assets
+          src="/resume.png"
           alt="Resume"
           className="w-full rounded-lg shadow-lg"
         />
-        {/* Animated Underline with Color Change and Removal on Hover End */}
         <a
           href="/resume.pdf"
+          download="Adam_Young_Resume.pdf"
           target="_blank"
           className="relative mt-4 text-white underline hover:text-blue-500 after:content-[''] after:absolute after:left-0 after:bottom-[-2px] after:w-full after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:scale-x-0 hover:after:scale-x-100 hover:after:origin-left hover:no-underline"
         >
--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -1,3 +1,5 @@
+import { Link } from "../components/shared/link";
+
 export function Work() {
   return (
     <>
@@ -33,18 +35,142 @@ export function Work() {
             This role allows me to apply my passion for software engineering to drive product improvements.
           </p>
         </div>
-        
-        <h1 className="text-2xl font-bold">Side Projects/Learning</h1>
-        <div className="p-4">
-          <h3 className="text-lg font-bold">
-            The Odin Project
-          </h3>
-          <p className="mt-4">
-            I am currently working through The Odin Project&apos;s full-stack development course using Node.js.
-            Through this, I have gained a deeper understanding of web technologies, including React, Express, and Node.js,
-            which has contributed to improvements in our .NET/Blazor codebase.
-          </p>
-        </div>
+      </div>
+      <h1 className="text-2xl font-bold -mt-2">Technologies</h1>
+      <div className="p-4">
+        <h2 className="text-xl font-bold">Web</h2>
+        <ul className="p-4">
+          <li>
+            <Link 
+              label={"ASP.NET Core"} 
+              url={"https://dotnet.microsoft.com/en-us/apps/aspnet"} 
+            />
+          </li>
+          <li>
+            <Link 
+              label={"Blazor"} 
+              url={"https://dotnet.microsoft.com/en-us/apps/aspnet/web-apps/blazor"} 
+            />
+          </li>
+          <li>
+            <Link 
+              label={"Node.js"} 
+              url={"https://nodejs.org/en"} 
+            />
+          </li>
+          <li>
+            <Link 
+              label={"React"} 
+              url={"https://react.dev/"} 
+            />
+          </li>
+          <li>
+            Vanilla HTML, CSS, and Javascript
+          </li>
+        </ul>
+      </div>
+      <div className="p-4 -mt-6">
+        <h2 className="text-xl font-bold">Automation & Controls</h2>
+        <ul className="p-4 list-disc">
+          <li>
+            <Link 
+              label={"Beckhoff Automation"} 
+              url={"https://www.beckhoff.com/en-us/"} 
+            />
+            <ul className="pl-8 list-disc">
+              <li>
+                <Link 
+                  label={"Programmable Logic Controller (PLC) using Structured Text"} 
+                  url={"https://www.beckhoff.com/en-us/products/automation/twincat/"} 
+                />
+              </li>
+              <li>
+                <Link 
+                  label={"Human Machine Interface (HMI)"} 
+                  url={"https://www.beckhoff.com/en-us/products/automation/twincat/texxxx-twincat-3-engineering/te2000.html"} 
+                />
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link 
+              label={"B&R Automation"} 
+              url={"https://www.br-automation.com/en-us/"} 
+            />
+            <ul className="pl-8 list-disc">
+              <li>
+                <Link 
+                  label={"Programmable Logic Controller (PLC) using C and Structured Text"} 
+                  url={"https://www.br-automation.com/en-us/products/software/automation-software/automation-studio/"} 
+                />
+              </li>
+              <li>
+                <Link 
+                  label={"mapp View"}
+                  url={"https://www.br-automation.com/en-us/products/software/mapp-technology/mapp-view/"}
+                />
+              </li>
+              <li>
+                <Link 
+                  label={"mapp Motion"}
+                  url={"https://www.br-automation.com/en-us/products/software/mapp-technology/mapp-motion/"}
+                />
+              </li>
+            </ul>
+          </li>
+          <li className="text-lg">
+            Communication Protocols
+            <ul className="pl-8 list-disc">
+              <li>
+                <Link 
+                  label={"EtherCAT"}
+                  url={"https://www.ethercat.org/en/technology.html"}
+                />
+              </li>
+              <li>
+                <Link 
+                  label={"Powerlink"}
+                  url={"https://www.br-automation.com/en/technologies/powerlink/"}
+                />
+              </li>
+              <li>
+                <Link 
+                  label={"OPC-UA"}
+                  url={"https://opcfoundation.org/about/what-is-opc/"}
+                />
+              </li>
+              <li>
+                <Link 
+                  label={"Profinet"}
+                  url={"https://us.profinet.com/technology/profinet/"}
+                />
+              </li>
+              <li>
+                <Link 
+                  label={"Modbus"}
+                  url={"https://www.modbus.org/"}
+                />
+              </li>
+              <li>
+                <Link 
+                  label={"ADS"}
+                  url={"https://infosys.beckhoff.com/english.php?content=../content/1033/tcinfosys3/11291871243.html&id="}
+                />
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <h1 className="text-2xl font-bold -mt-2">Side Projects/Learning</h1>
+      <div className="p-4">
+        <h3 className="text-lg font-bold">
+          The Odin Project
+        </h3>
+        <p className="mt-4">
+          I am currently working through The Odin Project&apos;s full-stack development course using Node.js.
+          Through this, I have gained a deeper understanding of web technologies, including React, Express, and Node.js,
+          which has contributed to improvements in our .NET/Blazor codebase.
+        </p>
       </div>
     </>
   );
