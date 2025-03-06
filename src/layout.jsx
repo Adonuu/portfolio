7829c4caad63f348b73b2c49148ca0e66741c788
Adam Young <adamyoung1414@gmail.com>
2025-03-05 21:12:49 -0500

    add page transition animations

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,13 +1,32 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useState, useLayoutEffect } from 'react';
 import { TopBar } from './components/shared/topbar';
 
 export function Layout() {
-    return (
-      <>
-        <TopBar />
-        <div className="ml-auto mr-auto max-w-4xl p-4">
-            <Outlet />
+  const location = useLocation();
+  const [isTransitioning, setIsTransitioning] = useState(true);
+
+  useLayoutEffect(() => {
+    setIsTransitioning(true);
+    const timeout = setTimeout(() => {
+      setIsTransitioning(false);
+    }, 300);
+    return () => clearTimeout(timeout);
+  }, [location]);
+
+  return (
+    <>
+      <TopBar />
+      <div className="ml-auto mr-auto max-w-4xl p-4">
+        <div
+          key={location.key}
+          className={`transition-all duration-300 ease-in-out ${
+            isTransitioning ? 'translate-y-4 opacity-0' : 'translate-y-0 opacity-100'
+          }`}
+        >
+          <Outlet />
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+}
